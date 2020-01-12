f7a9109e5f5695edcc34f2e31cba8377ee17e43b
Czar Yobero <czar.yobero@Czars-MacBook-Pro.local>
2020-01-12 10:08:03 -0800

    added new fields to Reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -6,8 +6,7 @@ from multiselectfield import MultiSelectField
 # Create your models here.
 class Review(models.Model):
     RATINGS_CHOICES = [
-        (1, 'Doo-doo'),
-        (2, 'Reggie Miller'),
+        (1, 'Doo-doo'), (2, 'Reggie Miller'),
         (3, 'Decent'),
         (4, 'Bomb'),
         (5, 'GOAT')
@@ -28,6 +27,23 @@ class Review(models.Model):
         (2, 'Munchies'),
         (3, 'Giggles'),
         (4, 'Low key kinda aroused'),
+        (5, 'Body High'),
+    ]
+
+    # What kind of activities go well with this strain?
+    ACTIVITIES_CHOICES = [
+        (1, 'Watching Movies'),
+        (2, 'Arts & Crafts'),
+        (3, 'Nature Walks'),
+        (4, 'Reading/Writing'),
+        (5, 'Lounging'),
+    ]
+
+    RELIEVES_CHOICES = [
+        (1, 'Chronic Pain'),
+        (2, 'Anxiety'),
+        (3, 'Stress'),
+        (4, 'Depression')
     ]
 
     rating = models.IntegerField(choices=RATINGS_CHOICES, blank=True, null=True)
@@ -35,6 +51,9 @@ class Review(models.Model):
     review = models.TextField(blank=True, null=True, default='Not yet reviewed.')
     reported_feelings = MultiSelectField(choices=REPORTED_FEELINGS_CHOICES, max_choices=8, blank=True, null=True)
     reported_effects = MultiSelectField(choices=REPORTED_EFFECTS_CHOICES, max_choices=4, blank=True, null=True)
+    reported_activities = MultiSelectField(choices=ACTIVITIES_CHOICES, max_choices=4, blank=True, null=True)
+    reported_reliefs = MultiSelectField(choices=RELIEVES_CHOICES, max_choices=4, blank=True, null=True)
+
 
     class Meta:
         ordering = ['-rating', 'flower']
