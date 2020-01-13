497c9dd9c3e0ef72f85a1821278e1fd07a214885
Czar Yobero <czar.yobero@Czars-MacBook-Pro.local>
2020-01-13 11:20:04 -0800

    renamed view vars

--- inventory/views.py
+++ inventory/views.py
@@ -7,11 +7,11 @@ from inventory.models import Grower
 # Create your views here.
 def product_info_view(request, slug):
     flower = get_object_or_404(Flower, slug=slug)
-    return render(request, 'product_info_view.html', {'flower': flower})
+    return render(request, 'product_info.html', {'flower': flower})
 
 
 def shop_home_view(request):
-    return render(request, 'shop_home_view.html')
+    return render(request, 'shop_home.html')
 
 
 def flowers_home_view(request):
@@ -19,7 +19,7 @@ def flowers_home_view(request):
     paginator = Paginator(flowers_list, 6)  # Show 6 flowers per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'flowers_home_view.html', {'flowers_list': flowers_list})
+    return render(request, 'flowers_home.html', {'flowers_list': flowers_list})
 
 
 def growers_home_view(request):
@@ -28,7 +28,7 @@ def growers_home_view(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'growers_home_view.html',
+    return render(request, 'growers_home.html',
             {'growers_list': growers_list,
              'page_obj': page_obj,
             })
@@ -36,4 +36,4 @@ def growers_home_view(request):
 
 def grower_info_view(request, slug):
     grower = get_object_or_404(Grower, slug=slug)
-    return render(request, 'grower_info_view.html', {'grower': grower})
+    return render(request, 'grower_info.html', {'grower': grower})
