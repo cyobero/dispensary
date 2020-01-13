1192adb6afd232fb2e90556a0a8ad7ef4fb5c7cd
Czar Yobero <czar.yobero@Czars-MacBook-Pro.local>
2020-01-13 13:40:00 -0800

    removed flower from ordering list

--- shopping_cart/admin.py
+++ shopping_cart/admin.py
@@ -4,7 +4,7 @@ from shopping_cart.models import ShoppingCart
 # Register your models here.
 class ShoppingCartAdmin(admin.ModelAdmin):
     ordering = ['customer', '-purchase_date']
-    list_display = ['customer', 'flower', 'purchase_date', 'quantity', 'unit_price']
-    search_fields = ['customer', 'flower', 'purchase_date']
+    list_display = ['customer', 'purchase_date', 'quantity', 'unit_price']
+    search_fields = ['customer', 'purchase_date']
 
 admin.site.register(ShoppingCart, ShoppingCartAdmin)
--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -5,10 +5,10 @@ from inventory.models import Flower
 # Create your models here.
 class ShoppingCart(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    flower = models.ManyToManyField(Flower, on_delete=models.CASCADE)
+    flower = models.ManyToManyField(Flower)
     quantity = models.IntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     purchase_date = models.DateField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-purchase_date', 'customer', 'flower']
+        ordering = ['-purchase_date', 'customer', ]
