1846d5c293e3169a58b5cab8ddf337e6b7bddb99
Czar Yobero <czar.yobero@Czars-MacBook-Pro.local>
2020-01-13 11:07:29 -0800

    changed var names for views

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from accounts.forms import RegisterForm, LoginForm
+from accounts.forms import RegisterForm
 
 
 # Create your views here.
-def register(request):
+def register_view(request):
     errors = []
 
     if request.method == 'POST':
@@ -21,10 +21,10 @@ def register(request):
         return redirect('home')
     else:
         form = RegisterForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'register_view.html', {'form': form})
 
 
-def signin(request):
+def login_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
