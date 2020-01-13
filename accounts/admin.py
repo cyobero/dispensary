2be76b70aa4f260c67f46b97167e484ca1fa4af3
Czar Yobero <czar.yobero@Czars-MacBook-Pro.local>
2020-01-13 13:19:32 -0800

    created admin for Customer and Plug models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from accounts.models import Customer, Plug
 
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    ordering = ['user']
+    search_fields = ['user']
+
+
+class PlugAdmin(admin.ModelAdmin):
+    ordering = ['user']
+    search_fields = ['user']
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Plug, PlugAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -11,12 +12,18 @@ class Customer(models.Model):
     image = models.ImageField(blank=True, null=True,
                               upload_to='accounts/customers/{}'.format(user))
     bio = models.TextField(max_length=600)
-
     twitter = models.URLField(max_length=80, blank=True, null=True)
     instagram = models.URLField(max_length=80, blank=True, null=True)
     youtube = models.URLField(max_length=80, blank=True, null=True)
-
     birth_date = models.DateField(blank=True, null=True)
+
+    def calculate_age(self):
+        current_date = datetime.datetime.today()
+        user_birthdate = '%s/%s/%s' % (str(self.birth_date.year),
+                                       str(self.birth_date.month), str(self.birth_date.day))
+        user_birthdate = datetime.datetime.strptime(user_birthdate, '%Y/%m/%d')
+        age = (current_date - user_birthdate)/365.2424
+        return age
 
 
 @receiver(post_save, sender=User)
@@ -43,20 +50,31 @@ class Plug(models.Model):
     image = models.ImageField(blank=True, null=True,
                               upload_to='accounts/customers/{}'.format(user))
     bio = models.TextField(max_length=600)
-
     twitter = models.URLField(max_length=80, blank=True, null=True)
     instagram = models.URLField(max_length=80, blank=True, null=True)
     youtube = models.URLField(max_length=80, blank=True, null=True)
-
     birth_date = models.DateField(blank=True, null=True)
-
     address_one = models.CharField(max_length=60, blank=True, null=True)
     address_two = models.CharField(max_length=40, blank=True, null=True)
     city = models.CharField(max_length=70)
     state = USStateField()
     zip_code = USZipCodeField()
+    plug_rating = models.IntegerField(
+        choices=RATINGS_CHOICES, blank=True, null=True)
 
-    plug_rating = models.IntegerField(choices=RATINGS_CHOICES, blank=True, null=True)
+    def calculate_age(self):
+        current_date = datetime.datetime.today()
+        user_birthdate = '%s/%s/%s' % (str(self.birth_date.year),
+                                       str(self.birth_date.month), str(self.birth_date.day))
+        user_birthdate = datetime.datetime.strptime(user_birthdate, '%Y/%m/%d')
+        age = (current_date - user_birthdate)/365.2424
+        return age
 
     class Meta:
         ordering = ['user', '-plug_rating']
+
+
+@receiver(post_save, sender=User)
+def create_plug_profile(sender, instance, created, **kwargs):
+    if created:
+        Plug.objects.create(user=instance)
