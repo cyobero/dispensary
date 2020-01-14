daa8e2bd1574041fbdeb693db12a6f53199ad7f9
Czar Yobero <czar.yobero@Czars-MacBook-Pro.local>
2020-01-14 06:37:36 -0800

    fixed login_view

--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from accounts.models import Customer
 from accounts.forms import RegisterForm
+from accounts.utils import old_enough
 
 
 # Create your views here.
 def register_view(request):
-    errors = []
-
     if request.method == 'POST':
         form = RegisterForm(request.POST)
+
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
-            confirm_password = form.cleaned_data.get('confirmed_password')
-            user = authenticate(username=username, password=raw_password)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            user = authenticate(username=username,
+                                password=password, email=email)
             login(request, user)
         else:
-            errors.appen
+            return render(request, 'register.html', {'form': form})
         return redirect('home')
     else:
         form = RegisterForm()
-    return render(request, 'register_view.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
 
 
 def login_view(request):
